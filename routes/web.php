b0303e579bea5b241922f1f862ef50a08d6fce33
bellasoniadwi <bellasoniadwi@gmail.com>
2022-04-20 21:34:59 +0700

    call route cetak_pdf

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ Route::resource('mahasiswa', MahasiswaController::class);
 Route::get('cari',[MahasiswaController::class, 'search']);
 Route::get('mahasiswa/nilai/{nim}',[MahasiswaController::class, 'nilai'])
     ->name('mahasiswa.nilai');
-
+Route::get('mahasiswa/nilai/{nim}/cetak_pdf', [MahasiswaController::class,'cetak_pdf'])
+    ->name('mahasiswa.cetak_pdf');
 Route::get('/', function () {
     return view('welcome');
 });
