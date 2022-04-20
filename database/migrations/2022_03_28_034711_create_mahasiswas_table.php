139181f0b9705d4e67923f889a0a617def7fe040
bellasoniadwi <bellasoniadwi@gmail.com>
2022-04-20 19:04:55 +0700

    dari jobsheet 9

---
+++ database/migrations/2022_03_28_034711_create_mahasiswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim', 10)->primary();
+            $table->string('nama', 50);
+            $table->date('tanggal_lahir');
+            $table->string('kelas', 10);
+            $table->string('jurusan', 50);
+            $table->string('no_handphone', 15);
+            $table->string('email',100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
